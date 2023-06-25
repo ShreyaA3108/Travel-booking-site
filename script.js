a354ef925dcd03dbf701ca42a034a35e63d19456
ShreyaA3108 <118830588+ShreyaA3108@users.noreply.github.com>
2023-06-25 10:50:54 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let searchBtn=document.querySelector(".searchbtn");
-let searchBar=document.querySelector("#search-bar");
+let whereBox=document.querySelector(".place");
+let HowManyBox=document.querySelector(".amount");
+let aTime=document.querySelector(".coming");
+let lTime=document.querySelector(".going");
+
+
+function procedure(){
+    if (whereBox.value===""|| HowManyBox.value==="" || aTime.value==="" || lTime.value===""){
+        alert("Please Fill all the blanks");
+    }
+    else{
+        alert("Booking Done");
+}
+}
 
